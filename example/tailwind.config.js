e7116d2c113ec704250dd4ce1095a4537fb0f46b
Yassine Bridi <ybridi@gmail.com>
2021-01-30 00:41:04 +0100

    chore: fix example tailwind.config.js

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -7,5 +7,21 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwind-direction').default,
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  corePlugins: {
+    borderRadius: false,
+    borderWidth: false,
+    clear: false,
+    divideWidth: false,
+    float: false,
+    margin: false,
+    padding: false,
+    space: false,
+    textAlign: false,
+    transformOrigin: false,
+  },
+};
